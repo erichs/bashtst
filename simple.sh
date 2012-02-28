ed03252641658a357ccc98d65b03190ff84c39c7
Erich Smith <heartquest@gmail.com>
2012-02-28 15:07:10 -0500

    simple script and tests

---
+++ simple.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 argument"
+  exit 1
+fi
+echo "arg: $1"
+exit 0
---
+++ test/test-simple.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+source init.inc
+
+REV=$1
+
+main() {
+    setup $REV
+
+    # run tests!
+    echo "running tests..."
+    (
+        label "no args gives help"
+        [ ! -z "$(./simple.sh | grep "Usage")" ] || flunk "Didn't get expected help with missing arg!"
+        pass
+    )
+    (
+        label "display argument"
+        [ ! -z "$(./simple.sh arg1 | grep "arg1")" ] || flunk "Didn't see arg in output!"
+        pass
+    )
+
+    cleanup
+}
+
+main
